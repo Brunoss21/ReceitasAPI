edc79333b984a064f7659a76dc211b0b6bb743be
Bruno Santos <bruno_silva1420@hotmail.com>
2024-09-19 21:22:06 -0300

    Ajustes Receita.java

--- src/main/java/com/fatecrl/receitas/model/Receita.java
+++ src/main/java/com/fatecrl/receitas/model/Receita.java
@@ -1,67 +1,61 @@
 package com.fatecrl.receitas.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Receita implements Serializable {
-    private long id;
+    private Long id;
     private String nome;
-    private String ingredientes;
-
-    public Receita(){
-        
-    }
-
-    public void Conta(long id){
-        this.id = id;
-    }
-
-    public long getId() {
+    private List<String> ingredientes;
+    private String instrucoes;
+    
+    public Long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public String getIngredientes() {
+    public List<String> getIngredientes() {
         return ingredientes;
     }
-
-    public void setIngredientes(String ingredientes) {
+    public void setIngredientes(List<String> ingredientes) {
         this.ingredientes = ingredientes;
     }
-
+    public String getInstrucoes() {
+        return instrucoes;
+    }
+    public void setInstrucoes(String instrucoes) {
+        this.instrucoes = instrucoes;
+    }
     @Override
-    public int hashCode(){
+    public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
     @Override
-    public boolean equals(Object obj){
-        if(this == obj)
+    public boolean equals(Object obj) {
+        if (this == obj)
             return true;
-        if(obj == null)
+        if (obj == null)
             return false;
-        if(getClass() != obj.getClass())
+        if (getClass() != obj.getClass())
             return false;
         Receita other = (Receita) obj;
-        if(id == null) {
+        if (id == null) {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
             return false;
         return true;
     }
+
 }
 
